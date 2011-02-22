1b1b9e19b56c40927292368b9d5dc5b52c64d0fa
dev@raincitysolutions.com <dev@raincitysolutions.com@151ac6ff-986a-871b-022e-23d3b99ee23e>
2011-02-22 14:40:02 +0000

    Minor change to task creation

--- trunk/2011_HostBot/MotorLimitWatchdog.cpp
+++ trunk/2011_HostBot/MotorLimitWatchdog.cpp
@@ -2,7 +2,8 @@
 
 #include "MotorLimitWatchdog.h"
 
-void MotorLimitWatchdogFunc(MotorLimitWatchdog * pWatchdog)
+LOCAL void
+MotorLimitWatchdogFunc(MotorLimitWatchdog * pWatchdog)
 {
 	pWatchdog->Run();
 }
@@ -12,7 +13,7 @@ MotorLimitWatchdog::MotorLimitWatchdog(
 		SpeedController * motor,
 		DigitalInput * highLimit,
 		DigitalInput * lowLimit):
-	Task("MotorLimitWatchDog", (FUNCPTR)&MotorLimitWatchdogFunc)
+	Task("MotorLimitWatchDog", (FUNCPTR)MotorLimitWatchdogFunc)
 {
 	m_motor = motor;
 	m_highLimit = highLimit;
@@ -38,5 +39,6 @@ MotorLimitWatchdog::Run()
 	{
 		if (m_highLimit->Get() || m_lowLimit->Get())
 			m_motor->Set(0);
+		Wait(0.5);
 	}
 }
