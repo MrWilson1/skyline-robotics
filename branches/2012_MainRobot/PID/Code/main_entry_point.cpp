1c62c10b31161b6addb4928323744845cfd1ec09
michael.lee.0x2a@gmail.com <michael.lee.0x2a@gmail.com@151ac6ff-986a-871b-022e-23d3b99ee23e>
2012-12-01 20:33:50 +0000

    Branch for experimental PID drive.

---
+++ branches/2012_MainRobot/PID/Code/main_entry_point.cpp
@@ -0,0 +1,38 @@
+/**
+ * @file main_entry_point.cpp
+ * 
+ * @brief The main entry point of the program.
+ * 
+ * @summary We can select at compile-time the robot class we want to run.
+ */
+
+#include "Profiles/MainRobot.h"
+#include "Profiles/PrototypeRobot.h"
+
+#define PROTOTYPE 1
+#define MAINROBOT 2
+/// Do not modify above
+
+// Pick which robot you want to run here.
+// Your choices:
+//
+// #define ROBOT MAINROBOT 
+// ...to run the main robot.
+//
+// #define ROBOT PROTOTYPE
+// ...to run the prototype robot.
+//
+// Pick which one you want and replace the line
+// below.
+
+//#define ROBOT MAINROBOT
+#define ROBOT PROTOTYPE
+
+/// Do not modify below
+
+#if ROBOT == MAINROBOT
+	START_ROBOT_CLASS(MainRobot);
+#elif ROBOT == PROTOTYPE
+	START_ROBOT_CLASS(PrototypeRobot);
+#endif
+
